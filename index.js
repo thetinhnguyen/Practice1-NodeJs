ce6cc459a3e8fa8ef434b08a7cd3f6c7ed76def5
NguyenTheTinh <tinh121999@.com>
2020-02-24 16:24:42 +0700

    Cookie of express: res.cookie method and req.cookies property

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ const express = require("express");
 
 let db=require('./db')
 const userRoute=require('./routes/user.route')
+const cookieParser = require('cookie-parser')
+ 
+
 const app = express();
 const port = 3000;
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(cookieParser())
+
 app.set("view engine", "pug");
 app.set("views", "./views");
 app.get("/", (req, res) => {
+  console.log(req.cookies.userID)
   res.render("index");
 });
+app.get('/cookie',(req,res,next)=>{
+  res.cookie('userID',12345)
+  res.send('Hello')
+})
 app.use('/users',userRoute)
 app.use(express.static('public'))
 app.listen(port, () => {
